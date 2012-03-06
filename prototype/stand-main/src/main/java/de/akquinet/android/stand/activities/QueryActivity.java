60592f346c618e87079421c54a5084166afe9ab3
Enrico Biermann <enrico.biermann@akquinet.de>
2012-03-06 13:41:10 +0100

    Added archetype template

---
+++ prototype/stand-main/src/main/java/de/akquinet/android/stand/activities/QueryActivity.java
@@ -0,0 +1,41 @@
+package de.akquinet.android.stand.activities;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import de.akquinet.android.roboject.RobojectActivity;
+import de.akquinet.android.roboject.annotations.InjectLayout;
+import de.akquinet.android.roboject.annotations.InjectView;
+
+@InjectLayout("query")
+public class QueryActivity extends RobojectActivity {
+    @InjectView private Button showResultButton;
+    @InjectView private EditText number1Input;
+    @InjectView private EditText number2Input;
+
+    @Override
+    public void onReady() {
+        super.onReady();
+
+        showResultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    int number1 = Integer.parseInt(number1Input.getText().toString());
+                    int number2 = Integer.parseInt(number2Input.getText().toString());
+
+                    Intent intent = new Intent();
+                    intent.setClass(getApplicationContext(), ResultActivity.class);
+                    intent.putExtra("number1", number1);
+                    intent.putExtra("number2", number2);
+                    startActivity(intent);
+                } catch (NumberFormatException e) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Please enter two integer values", 3);
+                    toast.show();
+                }
+            }
+        });
+    }
+}
---
+++ prototype/stand-main/src/main/java/de/akquinet/android/stand/activities/ResultActivity.java
@@ -0,0 +1,37 @@
+package de.akquinet.android.stand.activities;
+
+import android.widget.TextView;
+import de.akquinet.android.roboject.RobojectActivity;
+import de.akquinet.android.roboject.annotations.InjectExtra;
+import de.akquinet.android.roboject.annotations.InjectLayout;
+import de.akquinet.android.roboject.annotations.InjectService;
+import de.akquinet.android.roboject.annotations.InjectView;
+import de.akquinet.android.stand.services.TestService;
+
+import static de.akquinet.android.stand.services.TestService.*;
+
+@InjectLayout("result")
+public class ResultActivity extends RobojectActivity {
+    @InjectService(clazz = TestService.class)
+    AdderService adderService;
+    
+    @InjectView TextView addResultText;
+    
+    @InjectExtra int number1;
+    @InjectExtra int number2;
+    
+    long result;
+    
+    @Override
+    public void onServicesConnected() {
+        super.onServicesConnected();
+        
+        result = adderService.add(number1, number2);
+    }
+
+    @Override
+    public void onReady() {
+        super.onReady();
+        addResultText.setText(String.valueOf(result));
+    }
+}
